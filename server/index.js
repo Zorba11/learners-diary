945f502eb3c06aff28bebd6ce653a6f293105214
Zorba11 <zorbathegreek111@gmail.com>
2021-01-09 18:03:24 +0530

    server ready, first connection to mongo cluster

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+// require('dotenv').config();
+
+// const mongoAdminUser = process.env.MONGO_ADMIN_USERNAME;
+// const mongoPassword = process.env.MONGO_PASSWORD;
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//mongodb cloud atlas
+
+const CONNECTION_URL = 'mongodb+srv://zorba11:zorba11123@cluster0.ahvru.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
+
+
+

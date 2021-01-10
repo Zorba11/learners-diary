04d42b587baddf5a8ef5c076bc6d34cf6281c9fc
Zorba11 <zorbathegreek111@gmail.com>
2021-01-11 00:55:53 +0530

    form field styles added, createpost implemented and verified

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,8 +2,26 @@ import * as api from '../api';
 
 //Action creators using redux-thunk
 
-const getPosts = () => async(dispatch) => {
-    const action = { type: 'FETCH_ALL', payload: [] }
+export const getPosts = () => async(dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
 
-    dispatch(action);
+        // const action = { type: 'FETCH_ALL', payload: [] }
+
+        // dispatch(action);
+    
+        dispatch({ type: 'FETCH_ALL', payload: data})
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data })
+    } catch (error) {
+        console.log(error.message);
+    }
 }
